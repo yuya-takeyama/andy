0a2739568d3aaf6521c2ef58d8f5a591c69f1747
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-03 00:56:20 +0900

    Refactoring.

--- lib/andy/app.rb
+++ lib/andy/app.rb
@@ -6,18 +6,25 @@ class Andy::App < ::Sinatra::Base
   set :root, Andy::ROOT_DIR
   set :views, File.expand_path('views', settings.root)
   set :config, YAML::load(open(File.expand_path('config/config.yml', settings.root)))
+  set :repos, settings.config['repositories']
 
   get '/:repo' do
     @repo_name = params[:repo]
     @title = @repo_name
-    repo_config = settings.config['repositories'][@repo_name]
-    @repo = ::Grit::Repo.new(repo_config['url'])
-    @branches = @repo.branches
+    @branches = repo.branches
     haml :repo
   end
 
   get '/' do
     @repos = settings.config['repositories']
     haml :index
+  end
+
+  def repo_config
+    settings.repos[params[:repo]]
+  end
+
+  def repo
+    ::Grit::Repo.new(repo_config['url'])
   end
 end
