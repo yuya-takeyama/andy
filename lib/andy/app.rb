2a48398995a650577f6c03551bb9c34c49e5ef53
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-03 18:38:11 +0900

    Add repository setup.

--- lib/andy/app.rb
+++ lib/andy/app.rb
@@ -1,5 +1,6 @@
 require 'andy'
 require 'yaml'
+require 'digest/sha1'
 
 class Andy::App < ::Sinatra::Base
   set :haml, {:format => :html5}
@@ -7,6 +8,11 @@ class Andy::App < ::Sinatra::Base
   set :views, File.expand_path('views', settings.root)
   set :config, YAML::load(open(File.expand_path('config/config.yml', settings.root)))
   set :repos, settings.config['repositories']
+
+  get '/:repo/*.apk' do
+    branch = params[:splat].join('/')
+    setup_worktree(params[:repo], branch)
+  end
 
   get '/:repo' do
     @repo_name = params[:repo]
@@ -26,5 +32,27 @@ class Andy::App < ::Sinatra::Base
 
   def repo
     ::Grit::Repo.new(repo_config['url'])
+  end
+
+  def setup_worktree(repo_name, branch)
+    repo_hash = ::Digest::SHA1.hexdigest(repo_config['url'])
+    dir = ::File.expand_path("tmp/repos/#{repo_name}/#{repo_hash}/#{branch}", settings.root)
+    ::FileUtils.rm_rf(dir) if File.exist? dir
+    ::FileUtils.mkdir_p(dir)
+    grit = ::Grit::Git.new('/tmp')
+    clone_option = {:quiet => true, :verbose => false, :progress => false, :branch => branch}
+    grit.clone(clone_option, repo_config['url'], dir)
+    put_local_properties(dir)
+    orig_dir = Dir.pwd
+    Dir.chdir(dir)
+    result = `ant release`
+    Dir.chdir(orig_dir)
+    result
+  end
+
+  def put_local_properties(dir)
+    open("#{dir}/local.properties", 'w') do |file|
+      file.puts "sdk.dir=#{settings.config['android']['sdk_dir']}"
+    end
   end
 end
