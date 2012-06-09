8417b9e5e86adf2fb0379e3d8de3c8c4de91d326
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-10 00:13:47 +0900

    Add ant.properties appending.

--- lib/andy/apk_builder.rb
+++ lib/andy/apk_builder.rb
@@ -1,6 +1,13 @@
 require 'digest/sha1'
 
 class Andy::ApkBuilder
+  ANT_PROPERTIES = {
+    'file'           => 'key.store',
+    'password'       => 'key.store.password',
+    'alias'          => 'key.alias',
+    'alias_password' => 'key.alias.password',
+  }
+
   def build(project, path, sdk_dir)
     repo = project.repo
     dir = ::File.expand_path("tmp/repos/#{project.id}/#{repo.hash}#{path}", settings.root)
@@ -8,6 +15,7 @@ class Andy::ApkBuilder
     ::FileUtils.mkdir_p(dir)
     repo.checkout(path, dir)
     put_local_properties(dir, sdk_dir)
+    append_ant_properties(dir, project.keystore_config)
     orig_dir = Dir.pwd
     Dir.chdir(dir)
     java_opt = "_JAVA_OPTIONS='-Dfile.encoding=UTF-8'"
@@ -19,6 +27,15 @@ class Andy::ApkBuilder
   def put_local_properties(dir, sdk_dir)
     open("#{dir}/local.properties", 'w') do |file|
       file.puts "sdk.dir=#{sdk_dir}"
+    end
+  end
+
+  def append_ant_properties(dir, config)
+    open(File.expand_path('ant.properties', dir), 'a') do |file|
+      file.print "\n# Appended by Andy\n"
+      ANT_PROPERTIES.each do |key, prop|
+        file.puts "#{prop}=#{config[key]}" if config.key? key
+      end
     end
   end
 end
--- lib/andy/project.rb
+++ lib/andy/project.rb
@@ -1,9 +1,10 @@
 class Andy::Project
-  attr_reader :id, :name
+  attr_reader :id, :name, :keystore_config
   attr_accessor :repo
 
   def initialize(id, config)
-    @id   = id
-    @name = config['name']
+    @id              = id
+    @name            = config['name']
+    @keystore_config = config['keystore'] || {}
   end
 end
