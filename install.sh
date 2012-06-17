2b437a8926379f04b6da2649f028b4a17daeccf9
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-17 18:02:47 +0900

    Fix JS concatnation.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 lessc ./vendor/bootstrap/less/bootstrap.less > public/css/bootstrap.css
 lessc ./vendor/bootstrap/less/responsive.less > public/css/bootstrap-responsive.css
-cat ./vendor/bootstrap/js/*.js > public/js/bootstrap.js
+cat ./vendor/bootstrap/js/bootstrap-{transition,alert,button,carousel,collapse,dropdown,modal,tooltip,popover,scrollspy,tab,typeahead}.js > public/js/bootstrap.js
